8aa8c3b834d0b7a580dbfbe5acf0144de60cf7bc
Pragades-Rajagopal <pragadesgoldsmith@outlook.com>
2022-04-29 00:34:35 +0530

    code fix

---
+++ README.md
@@ -0,0 +1,21 @@
+# Showroom application
+
+This is a basic CRUD application developed using Django Framework
+
+### About application
+
+* Built using ORM technique
+* CRUD operations are handled in Admin page by the admin
+* In the application mainpage, user can view the details of the cars, search cars and showrooms
+
+### Database 
+
+SQLite database is used which is the default databse that comes with Django. (Due to some issues, Postgres db is not used)
+
+### Endpoint
+
+To access the application, hit below point
+```
+localhost:port/app
+```
+
--- showroom/carsapp/views.py
+++ showroom/carsapp/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render
+from django.http import Http404
 from .models import Showroom, Cars
 
 def index(request):
     cars_list = Cars.objects.order_by('-ratings')
-    showroom_list = Showroom.objects.all()
-    results = {'cars_list': cars_list, 'showroom_list': showroom_list}
+    showroom_list_sel = Showroom.objects.all()
+    cars_list_sel = Cars.objects.all().values("car_name").distinct()
+    results = {'cars_list': cars_list, 'cars_list_sel': cars_list_sel, 'showroom_list_sel': showroom_list_sel}
     # print(results)
     return render(request, 'carsapp/index.html', results)
 
@@ -22,12 +23,21 @@ def detail(request, car_id):
 def searchcar(request):
     car_name = request.POST.get('car_name')
     try:
-        car = Cars.objects.get(car_name=car_name)
-        showroom = Showroom.objects.get(pk=car.showroom_id)
+        car = Cars.objects.filter(car_name=car_name)
+        # print('car---', car)
+
+        values = []
+        for i in range(0, car.count()):
+            # print('for---', car[i].showroom_id)
+            result = Showroom.objects.get(pk=car[i].showroom_id)
+            values.append({'showroom_name': result.showroom_name, 'city': result.city, 'car': car[i]})
+        # print('showroom---', values)
+
     except (KeyError, Cars.DoesNotExist):
         raise Http404('Car does not exist in the application')
 
-    return render(request, 'carsapp/searchcar.html', {'car': car, 'showroom': showroom})
+    return render(request, 'carsapp/searchcar.html', {'values': values})
+
 
 def searchsr(request):
     sr_name = request.POST.get('sr_name')
